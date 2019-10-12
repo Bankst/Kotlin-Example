de640bfaa2b0f4f37181ccb19ae06674593adc7d
Matthew Morley <matthew.morley.ca@gmail.com>
2019-10-12 10:21:10 -0700

    Update Robot.kt

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -1,7 +1,12 @@
 
 package frc.robot
 
+import edu.wpi.first.wpilibj.experimental.command.CommandScheduler
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import frc.robot.subsystems.drive.DriveSubsystem
 import frc.robot.subsystems.example.ExampleSubsystem
+import frc.robot.subsystems.intake.Intake
+import frc.robot.subsystems.superstructure.Superstructure
 import org.ghrobotics.lib.wrappers.FalconTimedRobot
 
 object Robot : FalconTimedRobot() {
@@ -9,9 +14,14 @@ object Robot : FalconTimedRobot() {
     override fun robotInit() {
         // the + operator just adds the FalconSubsystem to the FalconSubsystemHandler
         +ExampleSubsystem
+        +Intake
+        +Superstructure
+        +DriveSubsystem
 
         // it's not necessary if the thing isn't a Subsystem or FalconSubsystem
         Controls
+
+        SmartDashboard.putData(CommandScheduler.getInstance())
 
         super.robotInit()
     }
